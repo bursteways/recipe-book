887aec43e9c8cd0a7474cf2ba502db089ed3ddbb
Stephen Burke <Sburke0708@gmail.com>
2020-04-15 09:31:31 -0400

    gross event binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 // Models
 import { Recipe } from '../recipe.model';
@@ -8,14 +8,14 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
+  @Output() onSelectRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('test', 'hello world', 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/16714-birthday-cake-600x600.jpg?ext=.jpg')
+    new Recipe('test', 'hello world', 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/16714-birthday-cake-600x600.jpg?ext=.jpg'),
+    new Recipe('test 2', 'hello world', 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/16714-birthday-cake-600x600.jpg?ext=.jpg')
   ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onRecipeSelected(recipe: Recipe) {
+    this.onSelectRecipe.emit(recipe);
   }
-
 }
